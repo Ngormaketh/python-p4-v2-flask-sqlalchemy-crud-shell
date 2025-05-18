652417b8308a47f3533bbf1a2ecf994ebb84fc95
Anyang Diing <anyang.ngor@student.moringaschool.com>
2025-05-19 00:39:07 +0300

    flask-sqlalchemycrudlab

--- server/app.py
+++ server/app.py
@@ -5,21 +5,27 @@ from flask_migrate import Migrate
 
 from models import db
 
-# create a Flask application instance 
+# Create a Flask application instance
 app = Flask(__name__)
 
-# configure the database connection to the local file app.db
+# Configure the database connection to the local file app.db
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 
-# configure flag to disable modification tracking and use less memory
+# Configure flag to disable modification tracking and use less memory
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# create a Migrate object to manage schema modifications
-migrate = Migrate(app, db)
-
-# initialize the Flask application to use the database
+# Initialize the Flask application to use the database
 db.init_app(app)
 
+# Create a Migrate object to manage schema modifications
+migrate = Migrate(app, db)
+
+# Optional: Add shell context for easier access in `flask shell`
+@app.shell_context_processor
+def make_shell_context():
+    from models import Pet
+    return {'db': db, 'Pet': Pet}
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
